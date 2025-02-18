41ec202a70d61f65b8905386703295a4d07a4e7b
unknown <lancedominiclabis@gmail.com>
2025-02-18 22:49:26 +0800

    migration activity

--- database/migrations/0001_01_01_000003_create_positions_table.php
+++ database/migrations/0001_01_01_000003_create_positions_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id('positionId');
+            $table->string('positionName');
+            $table->decimal('rate', 8,2);
+            $table->dateTime('createdAt', precision: 0);
+            $table->dateTime('updatedAt', precision: 0);
+
+
         });
     }
 
@@ -23,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('positions');
     }
 };
